149045a0471371f48474daa92447ff84b1165135
Ilshat <huzin.ilshat.91@mail.ru>
2023-12-03 02:54:51 +0500

    С#_course.Lesson_1

---
+++ C#_Course/C#_course.Lesson_1/Program.cs
@@ -0,0 +1,96 @@
+﻿// comment
+
+/*comment
+  comment
+  comment*/
+
+//  ctrl+/
+
+//Console.WriteLine(10);
+// string f = "Привет";
+// Console.Write(f);
+// Console.Write(f);
+
+// Console.Write("введите число: ");
+// int f = Convert.ToInt32(Console.ReadLine());
+// Console.Write(f + 10);
+
+// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+// Console.Clear();
+// Console.Write("введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int squa = b * b;
+
+// if (a == squa)
+// {
+//   Console.Write("Yes");
+// }
+// else 
+// {
+//   Console.Write("No");
+// }
+
+// Второй вариант решения.
+// Console.Clear();
+// Console.Write("введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+
+// if (a == b * b)
+// {
+//   Console.Write("Yes");
+// }
+// else 
+// {
+//   Console.Write("No");
+// }
+
+
+
+// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+// Console.Clear();
+// Console.Write("введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int i = n * (-1);
+// while (i <= n)
+// {
+//   Console.Write(i + " ");
+//   i ++;
+// }
+
+
+
+// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+// Console.Clear();
+// Console.Write("введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = n / 100;
+// int n3 = n % 10;
+// Console.WriteLine(n1 + n3);
+
